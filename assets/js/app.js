348ac2a1f5e422377fb91088bd1d0179e6633df2
Bui Quang Vinh <top.xom.ray@gmail.com>
2022-07-12 17:02:04 +0700

    Init

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+
+
+
+class Customer{
+    constructor(id, fullName, email , phone , adress, balance = 0, deleted = 0){
+            this.id = id
+            this.fullName = fullName
+            this.email = email
+            this.phone= phone
+            this.adress = adress
+            this.balance = balance
+            this.deleted = deleted
+        
+    }
+}
